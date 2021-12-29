c5ff4594aa3f3b0f7587f65bf601d9fea90ffc35
jdtan <jdtan33@gmailcom>
2021-12-28 23:19:16 -0800

    add image to exports

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd ? '/jdtan.github.io/' : '',
+  images: {
+    loader: 'imgix',
+    path: '',
+  },
 }
