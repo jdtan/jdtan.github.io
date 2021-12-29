8ff958561562bc6a3cca8ee1d9b0608673fca06b
jdtan <jdtan33@gmailcom>
2021-12-28 20:27:13 -0800

    add cdn

--- src/next.config.js
+++ src/next.config.js
@@ -1,3 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  reactStrictMode: true,
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/jdtan/jdtan.github.io/gh-pages/' : '',
 }
